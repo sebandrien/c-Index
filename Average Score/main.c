b6f2ab82f36820d23bba78ffdc0e2bd75ea4fb0b
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-07-17 22:05:40 -0400

    Update main.c

--- Average Score/main.c
+++ Average Score/main.c
@@ -5,14 +5,16 @@ int main ()
    float score_1, score_2, score_3, score_4, score_5;
    float sum, average;
     
-   printf ("Enter the scores for 5 subjects separated by a space: "); 
+   printf ("Enter the scores of five subjects: "); 
+   printf ("\n"); 
+   
    scanf ("%f %f %f %f %f", &score_1, &score_2, &score_3, &score_4, &score_5);
    
    sum = score_1 + score_2 + score_3 + score_4 + score_5;
    average = sum / 5;
    
    printf ("Total score: %f\n", sum);
-   printf ("Percentage: %.6f %%", average);
+   printf ("Percentage: %.2f %%", average);
 
    return 0;
 }
