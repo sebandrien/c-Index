5650d1e4d405bd7716a9036bfa3e46bbac96343e
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-07-22 16:53:37 -0400

    Create main.c

---
+++ Temperature Converter/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   float f, c;
+    
+   printf ("Enter the temperature in Fahrenheit: "); 
+   scanf ("%f", &f);
+   
+   c = (f - 32) * 5/9;
+   
+   printf ("The temperature in Celsius is: %.2f", c);
+
+   return 0;
+}
