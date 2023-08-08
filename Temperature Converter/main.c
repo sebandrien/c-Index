9a3d9562e40e3129f19168bfb9c1efed544de43e
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-08-08 19:15:29 -0400

    Update main.c

--- Temperature Converter/main.c
+++ Temperature Converter/main.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-   float f, c;
+  float f, c;
     
-   printf ("Enter the temperature in Fahrenheit: "); 
-   scanf ("%f", &f);
+  printf ("Enter the temperature in Fahrenheit: "); 
+  scanf ("%f", &f);
    
-   c = (f - 32) * 5/9;
+  c = (f - 32) * 5/9;
    
-   printf ("The temperature in Celsius is: %.2f", c);
+  printf ("The temperature in Celsius is: %.2f", c);
 
-   return 0;
+ return 0;
 }
