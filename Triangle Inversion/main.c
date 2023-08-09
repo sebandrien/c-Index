faf68b399bf5b12d893a1f764bdc3ac3230b0330
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-08-09 14:40:58 -0400

    Update main.c

--- Triangle Inversion/main.c
+++ Triangle Inversion/main.c
@@ -13,27 +13,23 @@ int updown_trianglePrinter(int n)
   {
     for(i = 0; i < n; i++) 
     {
-
-        for(j = 0; j < n - i; j++) 
+      for(j = 0; j < n - i; j++) 
         {
-            printf("*");
+          printf("*");
         }
-       
-        printf("\n");
+       printf("\n");
     }
     
      for (i = 2; i <= n; ++i) 
-     {
-         
+     {  
       for (j = 1; j <= i; ++j) 
       {
-         printf("*");
-         
+        printf("*");
       }
-      printf("\n");      
-   }   
+        printf("\n");      
+     }   
    ++x;
-    }   
+  }   
  return 0;
 }
     
@@ -45,11 +41,10 @@ int main()
   {   
    scanf ("%d", &n);
    if (n < 0)
-         {
-             break;
-         }
-         updown_trianglePrinter(n);
-        
+    {
+      break;
+    }
+    updown_trianglePrinter(n);  
     }
   printf("Total triangles printed : %d", x);
 }
