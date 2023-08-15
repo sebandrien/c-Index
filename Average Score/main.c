665af79c9dc40ecdc94b72113f12f6daa63edfef
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-08-15 06:11:07 -0400

    Update main.c

--- Average Score/main.c
+++ Average Score/main.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-int main ()
-{
-   float score_1, score_2, score_3, score_4, score_5;
-   float sum, average;
-    
-   printf ("Enter the scores of five subjects: "); 
-   printf ("\n"); 
-   
-   scanf ("%f %f %f %f %f", &score_1, &score_2, &score_3, &score_4, &score_5);
-   
-   sum = score_1 + score_2 + score_3 + score_4 + score_5;
-   average = sum / 5;
-   
-   printf ("Total score: %f\n", sum);
-   printf ("Average: %f", average);
-   
- return 0;
+int main() {
+    float score_1, score_2, score_3, score_4, score_5;
+    float sum, average;
+
+    printf("Enter the scores of five subjects:\n"); 
+
+    scanf("%f %f %f %f %f", &score_1, &score_2, &score_3, &score_4, &score_5);
+
+    sum = score_1 + score_2 + score_3 + score_4 + score_5;
+    average = sum / 5;
+
+    printf("Total score: %f\n", sum);
+    printf("Average: %f", average);
+
+    return 0;
 }
