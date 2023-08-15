e58a9e59f7e31313cce2e5dbeb93fc1e493cfa2a
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-08-15 05:49:56 -0400

    Update main.c

--- Reverse Number/main.c
+++ Reverse Number/main.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 
-int main () 
+int main() 
 {
-  int num,d1,d2,d3,d4,d5,sum;
+    int num, d1, d2, d3, d4, d5, sum;
     
-  printf("Enter a five digit number : "); 
-  scanf("%d", &num);
+    printf("Enter a five-digit number: "); 
+    scanf("%d", &num);
     
-  d1 = num % 10;
-  num = num / 10;
+    d1 = num % 10;
+    num /= 10;
     
-  d2 = num % 10;
-  num = num / 10;
+    d2 = num % 10;
+    num /= 10;
     
-  d3 = num % 10;
-  num = num / 10;
+    d3 = num % 10;
+    num /= 10;
     
-  d4 = num % 10;
-  num = num / 10;
+    d4 = num % 10;
+    d5 = num / 10;  
     
-  d5 = num;
-  num = d1 * 10000 + d2 *1000 + d3 *100 +d4 *10 + d5;
-
-  printf("Reverse Number: %d\n", num);
+    num = d1 * 10000 + d2 * 1000 + d3 * 100 + d4 * 10 + d5;
+    printf("Reverse Number: %d\n", num);
     
-  sum =  d1 + d2 + d3 + d4 + d5;
+    sum = d1 + d2 + d3 + d4 + d5;
+    printf("\nSum of digits: %d + %d + %d + %d + %d = %d\n", d5, d4, d3, d2, d1, sum);
     
-  printf("\nSum of digits: %d + %d + %d + %d + %d = %d\n", d5, d4, d3, d2, d1, sum);
-    
- return 0;
+    return 0;
 }
