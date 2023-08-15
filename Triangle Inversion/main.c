2bd25cc46234522b42595f77c903530e50f96d32
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-08-15 06:09:22 -0400

    Update main.c

--- Triangle Inversion/main.c
+++ Triangle Inversion/main.c
@@ -2,49 +2,43 @@
 
 int x = 0;
 
-int updown_trianglePrinter(int n)
-{   
-  int i;
-  int j;
+int updown_trianglePrinter(int n) {   
+    int i, j;
  
-  printf("Number of lines: %d\n", n);
+    printf("Number of lines: %d\n", n);
      
-  if (n >0) 
-  {
-    for(i = 0; i < n; i++) 
-    {
-      for(j = 0; j < n - i; j++) 
-        {
-          printf("*");
+    if (n > 0) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i; j++) {
+                printf("*");
+            }
+            printf("\n");
         }
-       printf("\n");
-    }
-    
-     for (i = 2; i <= n; ++i) 
-     {  
-      for (j = 1; j <= i; ++j) 
-      {
-        printf("*");
-      }
-        printf("\n");      
-     }   
-   ++x;
-  }   
- return 0;
+        
+        for (i = 2; i <= n; ++i) {  
+            for (j = 1; j <= i; ++j) {
+                printf("*");
+            }
+            printf("\n");      
+        }   
+        
+        ++x; 
+    }   
+    return 0;
 }
     
-int main()
-{
-  int n;
+int main() {
+    int n;
 
-  while (1)
-  {   
-   scanf ("%d", &n);
-   if (n < 0)
-    {
-      break;
+    while (1) {   
+        scanf("%d", &n);
+        
+        if (n < 0) {
+            break;
+        }
+        
+        updown_trianglePrinter(n);  
     }
-    updown_trianglePrinter(n);  
-    }
-  printf("Total triangles printed : %d", x);
+    
+    printf("Total triangles printed: %d", x);
 }
