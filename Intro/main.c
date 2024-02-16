86449f7ad269d5521e2ad7e3d9f5fd11c29e0550
sebandrien <129107648+sebandrien@users.noreply.github.com>
2024-02-15 21:51:17 -0500

    Create main.c

---
+++ Intro/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+
+  printf("The current month is January.\n");
+  printf("The current year is 2022.\n");
+  printf("I am a student at the University of Central Florida.\n");
+  printf("This Spring 2022, I am taking COP3502C Computer Science 1 with Dr. Steinberg.\n");
+  printf("I am super excited to learn foundational CS topics this semester!");
+  
+  return 0;
+}
